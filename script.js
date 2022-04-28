752726bcc684393348a526ca9c113a673f99ad14
TayTufenkjian <taytuf@gmail.com>
2022-04-28 11:03:56 -0400

    Create function to play a round
    Compare player and computer selections; return the result

--- script.js
+++ script.js
@@ -9,4 +9,66 @@ function computerPlay() {
     return options[randomIndex];
 }
 
-console.log(computerPlay());
+
+function playRound(playerSelection, computerSelection) {
+
+    // Initialize winner and results variables
+    let isWinner;
+    let result;
+
+    // Create error message in case something goes wrong
+    let game_error = 'Something went wrong. Please try again.';
+
+    // Check for a tie
+    if (playerSelection === computerSelection) {
+        return 'You tied! Play again.';
+    } else { 
+        // If there's no tie, compare player and computer selections 
+        if (playerSelection === 'rock') {
+            if (computerSelection === 'scissors') {
+                isWinner = true;
+            } else if (computerSelection === 'paper') {
+                isWinner = false;
+            } else {
+                return game_error;
+            }
+        } else if (playerSelection === 'scissors') {
+            if (computerSelection === 'paper') {
+                isWinner = true;
+            } else if (computerSelection === 'rock') {
+                isWinner = false;
+            } else {
+                return game_error;
+            }
+        } else if (playerSelection === 'paper') {
+            if (computerSelection === 'rock') {
+                isWinner = true;
+            } else if (computerSelection === 'scissors') {
+                isWinner = false;
+            } else {
+                return game_error;
+            }
+        } else {
+            return game_error;
+        }
+
+        // Return the winner/loser result
+        playerSelectionFormatted = playerSelection.replace(playerSelection[0], playerSelection[0].toUpperCase());
+        computerSelectionFormatted = computerSelection.replace(computerSelection[0], computerSelection[0].toUpperCase());
+
+        if (isWinner) {
+            result = `You win! ${playerSelectionFormatted} beats ${computerSelectionFormatted}.`;
+        } else {
+            result = `You lose! ${computerSelectionFormatted} beats ${playerSelectionFormatted}.`;
+        }
+        return result;
+    }
+}
+
+let playerSelection ="scissors";
+let computerSelection = computerPlay();
+
+console.log(playerSelection);
+console.log(computerSelection);
+
+console.log(playRound(playerSelection, computerSelection));
